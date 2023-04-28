7d4746243eeca1540e14461479875bef76a88aaf
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2023-04-28 14:19:08 +0800

    fix: warning

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,7 +4,7 @@ import { store, useLowduxSelector } from 'lowdux';
 export default function Sidebar() {
   const open = useLowduxSelector<boolean>('sidebarOpen');
   return (
-    <Drawer open={open} onClose={() => store.set('sidebarOpen', false)}>
+    <Drawer open={open || false} onClose={() => store.set('sidebarOpen', false)}>
       <List>
         <ListItemButton>JSON</ListItemButton>
       </List>
