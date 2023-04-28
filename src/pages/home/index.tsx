2cac1ee933000ee5d4b3aa8d72763db02c6a5d3d
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2023-04-28 13:36:30 +0800

    redirect home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,5 @@
-import { Box, Container, Grid } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-import JsonCard from '../json/JsonCard';
+import { Navigate } from 'react-router-dom';
 
 export default function Home() {
-  const { t } = useTranslation();
-  return (
-    <Box py={4}>
-      <Container>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <JsonCard />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <JsonCard />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  );
+  return <Navigate to="/json" />;
 }
