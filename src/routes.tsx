521ab9fedf73c417854d91b2e5a08c8b5540d545
Guo Yunhe <i@guoyunhe.me>
2023-09-16 20:38:28 +0800

    fix build

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,33 +1,13 @@
 import { lazy } from 'react';
 import { RouteObject } from 'react-router-dom';
 import AppLayout from './layouts/app';
-import LandingLayout from './layouts/landing';
-import NotFound from './pages/error/not-found';
-import Home from './pages/home';
 
 const JsonPage = lazy(() => import('./pages/json'));
 const XmlPage = lazy(() => import('./pages/xml'));
 const ColorPage = lazy(() => import('./pages/color'));
 const SvgPage = lazy(() => import('./pages/svg'));
 
-const AboutPage = lazy(() => import('./pages/about'));
-
 const routes: RouteObject[] = [
-  {
-    path: '/',
-    element: <LandingLayout />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: 'about',
-        element: <AboutPage />,
-      },
-    ],
-  },
   {
     path: '/',
     element: <AppLayout />,
