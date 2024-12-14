4d28ad689df2e00d8313ddb1ea9a1d947dd28ab8
Guo Yunhe <i@guoyunhe.me>
2024-12-14 23:19:36 +0800

    refactor: page layout

--- src/pages/json-to-url/index.tsx
+++ src/pages/json-to-url/index.tsx
@@ -2,6 +2,7 @@ import { useLocalStorage } from '@guoyunhe/react-storage';
 import { Box, FormControlLabel, Switch } from '@mui/material';
 import { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import sortKeysFunc from 'sort-keys';
 import Editor from '~/components/editor';
 import ToolPage from '~/components/tool-page';
 import jsonDeepParse from '~/utils/jsonDeepParse';
@@ -13,15 +14,19 @@ export default function JsonToUrlPage() {
   const [input, setInput] = useState(placeholder);
 
   const [deepParse, setDeepParse] = useLocalStorage('json_to_url_deep_parse', true);
+  const [sortKeys, setSortKeys] = useLocalStorage('json_to_url_sort_keys', true);
 
   const output = useMemo(() => {
-    const data: any = deepParse ? jsonDeepParse(input) : jsonShallowParse(input);
+    let data: any = deepParse ? jsonDeepParse(input) : jsonShallowParse(input);
+    if (sortKeys && typeof data === 'object') {
+      data = sortKeysFunc(data, { deep: true });
+    }
     const params = new URLSearchParams();
     Object.entries(data).forEach(([key, val]) => {
       params.set(key, typeof val === 'object' ? JSON.stringify(val) : String(val));
     });
     return params.toString();
-  }, [input, deepParse]);
+  }, [deepParse, input, sortKeys]);
 
   return (
     <ToolPage
@@ -34,10 +39,14 @@ export default function JsonToUrlPage() {
             }
             label={t('Deep parse')}
           />
+          <FormControlLabel
+            control={<Switch checked={sortKeys} onChange={(e, v) => setSortKeys(v)} />}
+            label={t('Sort properties')}
+          />
         </Box>
       }
     >
-      <Box sx={{ flex: '1 1 auto', display: 'flex', overflow: 'hidden', gap: 1 }}>
+      <Box sx={{ flex: '1 1 100%', display: 'flex', overflow: 'hidden', gap: 1 }}>
         <Editor
           title={t('Input')}
           value={input}
@@ -45,7 +54,12 @@ export default function JsonToUrlPage() {
           language="js"
           sx={{ flex: '1 1 50%' }}
         />
-        <Editor title={t('Output')} value={output} language="txt" sx={{ flex: '1 1 50%' }} />
+        <Editor
+          title={t('Output')}
+          value={output}
+          language="txt"
+          sx={{ flex: '1 1 50%', overflow: 'hidden' }}
+        />
       </Box>
     </ToolPage>
   );
--- src/pages/url-to-json/index.tsx
+++ src/pages/url-to-json/index.tsx
@@ -2,6 +2,7 @@ import { useLocalStorage } from '@guoyunhe/react-storage';
 import { Box, FormControlLabel, Switch } from '@mui/material';
 import { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import sortKeysFunc from 'sort-keys';
 import Editor from '~/components/editor';
 import ToolPage from '~/components/tool-page';
 import jsonDeepParse from '~/utils/jsonDeepParse';
@@ -13,15 +14,19 @@ export default function UrlToJsonPage() {
   const [input, setInput] = useState(placeholder);
 
   const [deepParse, setDeepParse] = useLocalStorage('url_to_json_deep_parse', true);
+  const [sortKeys, setSortKeys] = useLocalStorage('url_to_json_sort_keys', true);
 
   const output = useMemo(() => {
     const params = new URLSearchParams(input);
-    const data: any = {};
+    let data: any = {};
     for (const [key, val] of params.entries()) {
       data[key] = deepParse ? jsonDeepParse(val) : val;
     }
+    if (sortKeys) {
+      data = sortKeysFunc(data, { deep: true });
+    }
     return JSON.stringify(data, null, 2);
-  }, [input, deepParse]);
+  }, [input, sortKeys, deepParse]);
 
   return (
     <ToolPage
@@ -33,6 +38,10 @@ export default function UrlToJsonPage() {
               <Switch checked={deepParse} onChange={(e) => setDeepParse(e.target.checked)} />
             }
             label={t('Deep parse')}
+          />
+          <FormControlLabel
+            control={<Switch checked={sortKeys} onChange={(e, v) => setSortKeys(v)} />}
+            label={t('Sort properties')}
           />
         </Box>
       }
